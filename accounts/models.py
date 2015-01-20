7c41218ca474069f6ac37d810afc6c921006e9d0
medfiras <hajjejfiras@gmail.com>
2015-01-20 11:54:02 +0100

    adding attribute models and fixing problems in user template

--- accounts/models.py
+++ accounts/models.py
@@ -7,17 +7,18 @@ from django.core.validators import URLValidator
 #from phonenumber_field.modelfields import PhoneNumberField
 
 class MyProfile(UserenaBaseProfile):
-	user = models.OneToOneField(User,unique=True,verbose_name=_('user'),related_name='my_profile')
-	birth_date = models.DateTimeField(blank=True, null=True)
+	user = models.OneToOneField(User,unique=True,verbose_name=_('user'),related_name='profile')
+	birth_date = models.DateField(blank=True, null=True)
 	address = models.TextField(blank=True, null=True)
 	#telephone = models.PhoneNumberField(blank=True)
 	about = models.TextField(blank=True, null=True)
-	#city = models.TextField(blank=True, null=True)
-	#GENDER_CHOICES = (('M', 'Male'),('F', 'Female'))
-	#gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-	# github = models.TextField(validators=[URLValidator()])
-	# facebook = models.TextField(validators=[URLValidator()])
-	# twitter = models.TextField(validators=[URLValidator()])
-	# googleplus = models.TextField(validators=[URLValidator()])
+	city = models.CharField(max_length=100,blank=True, null=True)
+	GENDER_CHOICES = (('M', 'Male'),('F', 'Female'))
+	gender = models.CharField(max_length=1, choices=GENDER_CHOICES,blank=True, null=True)
+	github = models.CharField(max_length=100,validators=[URLValidator()],blank=True, null=True)
+	facebook = models.CharField(max_length=100,validators=[URLValidator()],blank=True, null=True)
+	twitter = models.CharField(max_length=100,validators=[URLValidator()],blank=True, null=True)
+	googleplus = models.CharField(max_length=100,validators=[URLValidator()],blank=True, null=True)
+	website = models.CharField(max_length=100,validators=[URLValidator()],blank=True, null=True)
 
 
