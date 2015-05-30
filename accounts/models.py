a26f3df162ceede0082bc0cfe0351100e790ecf2
medfiras <hajjejfiras@gmail.com>
2015-05-30 10:28:20 +0100

    adding group models

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,23 @@
 from django.db import models
-
+import uuid
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 from userena.models import UserenaBaseProfile
 from django.core.validators import URLValidator
+from django.contrib import admin
 #from phonenumber_field.modelfields import PhoneNumberField
+
+class Group(models.Model):
+	"""docstring for Groupe"""
+	uuid = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=100, unique=True, blank=False, null=False)
+	description = models.TextField(blank=True, null=True)
+	def __unicode__(self):
+		return self.name
 
 class MyProfile(UserenaBaseProfile):
 	user = models.OneToOneField(User,unique=True,verbose_name=_('user'),related_name='profile')
+	group = models.ForeignKey(Group,blank=True, null=True)
 	birth_date = models.DateField(blank=True, null=True)
 	address = models.TextField(blank=True, null=True)
 	#telephone = models.PhoneNumberField(blank=True)
@@ -22,3 +32,6 @@ class MyProfile(UserenaBaseProfile):
 	website = models.CharField(max_length=100,validators=[URLValidator()],blank=True, null=True)
 
 
+class GroupAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    list_display = ["name"]
